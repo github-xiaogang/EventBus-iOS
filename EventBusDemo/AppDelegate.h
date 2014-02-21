1fccb3e2517aa7f7e4cae26f88b81d1df4483282
张小刚 <zhang@zhangxiaogangdeMac.local>
2014-02-21 10:46:06 +0800

    Initial Commit

---
+++ EventBusDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EventBusDemo
+//
+//  Created by 张小刚 on 14-2-21.
+//  Copyright (c) 2014年 duohuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EventBusDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EventBusDemo
+//
+//  Created by 张小刚 on 14-2-21.
+//  Copyright (c) 2014年 duohuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
